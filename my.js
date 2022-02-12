9f461c84e5283df4b1330c17849d790814e33464
Superman <1601386403@qq.com>
2022-02-12 14:58:09 +0800

    哈哈

--- my.js
+++ my.js
@@ -1,5 +1,6 @@
+<div>
 <botton onclick="alert('讨厌你点我干嘛');">点我一下</botton>
-<botton onclick="alert('讨厌你点我干嘛');">点我一下</botton>
-<botton onclick="alert('讨厌你点我干嘛');">点我一下</botton>
-<botton onclick="alert('讨厌你点我干嘛');">点我一下</botton>
-<botton onclick="alert('讨厌你点我干嘛');">点我一下</botton>
+<botton onclick="alert('讨你点我干嘛');">点我一下</botton>
+<botton onclick="alert('讨厌点我干嘛');">点我一下</botton>
+<botton onclick="alert('讨厌点我干嘛');">点我一下</botton>
+<botton onclick="alert('讨厌你点干嘛');">点我一下</botton></div>
